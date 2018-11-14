81401a8330be7fb889766c8285822957ac8264e0
Josep <pujol.rabat@gmail.com>
2018-11-14 14:08:57 +0000

    added final change implementation and different coins denomination eur and usd

--- vending_machine.py
+++ vending_machine.py
@@ -1,21 +1,15 @@
 from biotest import *
 
-# coins available: 100, 50, 20, 10, 5, 2, 1
 
-def get_change(amount):
-    list_avail_coins = [100, 50, 20, 10, 5, 2, 1, ]
-    
-    if amount ==0: 
-        return []
-    
-    if amount in list_avail_coins:  
-        return [amount]
+usd_coins = [100, 50, 25, 10, 5, 1, ]
+eur_coins = [100, 50, 20, 10, 5, 2, 1, ]
 
+def get_change(amount, coins=eur_coins):
     change = []
-    for coin in list_avail_coins:
-        if coin <= amount:
+    for coin in coins:
+        while coin <= amount:
+            amount -= coin
             change.append(coin)
-            
     return change
 
 
@@ -29,8 +23,10 @@ test_are_equal(get_change(10), [10])
 test_are_equal(get_change(20), [20])
 test_are_equal(get_change(50), [50])
 test_are_equal(get_change(100), [100])
-test_are_equal(get_change(3), [2, 1,])
+test_are_equal(get_change(3), [2, 1, ])
 test_are_equal(get_change(7), [5, 2, ])
+test_are_equal(get_change(9), [5, 2, 2, ])
+test_are_equal(get_change(35, usd_coins), [25, 10, ])
 
 
 print('All tests pass!')
